4998e9e9d27b88f5dd34740437994e01617031c3
theodosisathanasakis <theathral@csd.auth.gr>
2020-04-15 03:37:18 +0200

    new layout in hospitals.html changes in covid19.html (to do: adjust height) delete unnecessary imports to do: function for dynamically change options and results

---
+++ health4u/static/health4u.js
@@ -0,0 +1,35 @@
+$(window).onload(function loadFilters() {
+    alert("alert!!!");
+    document.getElementById("onDutyCheckbox").checked = true;
+    document.getElementById("departmentDiv").style.display = "none";
+    document.getElementById("dateDiv").style.display = "block";
+    // current date
+});
+
+function onDutyChange(onDuty) {
+
+    if (onDuty.checked) {
+        document.getElementById("departmentDiv").style.display = "none";
+        document.getElementById("dateDiv").style.display = "block";
+    } else {
+        document.getElementById("departmentDiv").style.display = "block";
+        document.getElementById("dateDiv").style.display = "none";
+    }
+
+}
+
+// function selectRegion(choice) {
+//
+//     var hospital = document.getElementById("hospital");
+//
+//     hospital.removeChild(hospital.firstChild)
+//
+//     switch (choice) {
+//         case "Athens": hospital.
+//             break;
+//         case "Patra": break;
+//         case "Thessaloniki": break;
+//     }
+// }
+
+
