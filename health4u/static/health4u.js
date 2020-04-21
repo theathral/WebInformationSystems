18b67e5284fb1f229308509c8ad8681587089f85
theodosisathanasakis <theathral@csd.auth.gr>
2020-04-21 21:25:43 +0200

    fix datepicker in hospitals.html merge selectFilter.js and health4u.js more comments more code refactoring

--- health4u/static/health4u.js
+++ health4u/static/health4u.js
@@ -1,3 +1,9 @@
+// Hospital Results
+$("*.btn-collapse").on('click', function () {
+    $(this).closest("*.card-header").next().collapse('toggle');
+});
+
+// Checkbox for On Duty Changes
 $("#onDutyCheckbox").on("change", function onDutyChange() {
     if ($(this).prop("checked")) {
         document.getElementById("departmentDiv").style.display = "none";
@@ -8,11 +14,132 @@ $("#onDutyCheckbox").on("change", function onDutyChange() {
     }
 });
 
-$("*.btn-collapse").on('click', function () {
-    $(this).closest("*.card-header").next().collapse('toggle');
-});
+// Date Options
+$(".datepicker").datepicker({
+    format: "dd/mm/yyyy",
+    startDate: "+0d",
+    endDate: "+100d",
+    maxViewMode: 1,
+    todayBtn: "linked",
+    autoclose: true,
+    todayHighlight: true
+}).datepicker("setDate",'now');
 
-$("#slideshow").carousel({
-    interval: false,
-    wrap: false
+// Static Filtering for Part 1
+$(".selectpicker").selectpicker();
+
+$('select.selectpicker').on('change', function () {
+
+    var region = $('#regionFilter option:selected').val().toString();
+    var hospital = $('#hospitalFilter option:selected').val().toString();
+
+    switch (region) {
+        case "0": // Athens
+            $("#AgiosAndreas").hide();
+            $("#AHEPA").hide();
+            $("#Attikon").show();
+            $("#Euaggelismos").show();
+
+            $("#AgiosAndreasCard").hide();
+            $("#AHEPACard").hide();
+            $("#AttikonCard").show();
+            $("#EuaggelismosCard").show();
+            break;
+        case "1": // Patra
+            $("#AgiosAndreas").show();
+            $("#AHEPA").hide();
+            $("#Attikon").hide();
+            $("#Euaggelismos").hide();
+
+            $("#AgiosAndreasCard").show();
+            $("#AHEPACard").hide();
+            $("#AttikonCard").hide();
+            $("#EuaggelismosCard").hide();
+
+            break;
+        case "2": // Thessaloniki
+            $("#AgiosAndreas").hide();
+            $("#AHEPA").show();
+            $("#Attikon").hide();
+            $("#Euaggelismos").hide();
+
+            $("#AgiosAndreasCard").hide();
+            $("#AHEPACard").show();
+            $("#AttikonCard").hide();
+            $("#EuaggelismosCard").hide();
+
+            break;
+        default:
+            $("#AgiosAndreas").show();
+            $("#AHEPA").show();
+            $("#Attikon").show();
+            $("#Euaggelismos").show();
+
+            $("#AgiosAndreasCard").show();
+            $("#AHEPACard").show();
+            $("#AttikonCard").show();
+            $("#EuaggelismosCard").show();
+
+            $("#regionFilter").val("");
+
+            break;
+    }
+
+    switch (hospital) {
+        case "0": // Agios Andreas
+            $("#Athens").hide();
+            $("#Patra").show();
+            $("#Thessaloniki").hide();
+
+            $("#AgiosAndreasCard").show();
+            $("#AHEPACard").hide();
+            $("#AttikonCard").hide();
+            $("#EuaggelismosCard").hide();
+
+            break;
+        case "1": // AHEPA
+            $("#Athens").hide();
+            $("#Patra").hide();
+            $("#Thessaloniki").show();
+
+            $("#AgiosAndreasCard").hide();
+            $("#AHEPACard").show();
+            $("#AttikonCard").hide();
+            $("#EuaggelismosCard").hide();
+
+            break;
+        case "2": // Attikon
+            $("#Athens").show();
+            $("#Patra").hide();
+            $("#Thessaloniki").hide();
+
+            $("#AgiosAndreasCard").hide();
+            $("#AHEPACard").hide();
+            $("#AttikonCard").show();
+            $("#EuaggelismosCard").hide();
+
+            break;
+        case "3": // Euaggelismos
+            $("#Athens").show();
+            $("#Patra").hide();
+            $("#Thessaloniki").hide();
+
+            $("#AgiosAndreasCard").hide();
+            $("#AHEPACard").hide();
+            $("#AttikonCard").hide();
+            $("#EuaggelismosCard").show();
+
+            break;
+        default:
+            $("#Athens").show();
+            $("#Patra").show();
+            $("#Thessaloniki").show();
+
+            $("#hospitalFilter").val("");
+
+            break;
+    }
+
+    $('.selectpicker').selectpicker('refresh');
+
 });
