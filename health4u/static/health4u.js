2b2e6dc9c8e79a9faba2ccd8f92468bd7473be5e
theodosisathanasakis <theathral@csd.auth.gr>
2020-04-23 16:03:19 +0200

    jQuery downgrade 3.5.0 -> 3.4.1

--- health4u/static/health4u.js
+++ health4u/static/health4u.js
@@ -18,7 +18,7 @@ $(".back-to-top").on("click", function () {
 // Hospital Results
 $("*.btn-collapse").on("click", function () {
     $(this).toggleClass("collapsed");
-    $(this).closest("*.card-header").next().collapse('toggle');
+    $(this).closest("*.card-header").next().collapse("toggle");
 });
 // #Hospital Results
 
