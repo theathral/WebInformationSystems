47fc0e836fafd59dbd7ac4f1c308261dbd6ba7b3
GiannisMak <ivmakris@csd.auth.gr>
2020-05-27 17:40:47 +0300

    Forgot Password Implementation

--- health4u/__init__.py
+++ health4u/__init__.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import flask
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from flask_restful import Api
@@ -44,7 +45,24 @@ def create_app(test_config=None):
 
     @app.route("/contact_us")
     def contact_us():
-        return render_template("contact_us.html", req_datetime=datetime.now())
+        if flask.request.method == 'GET':
+            return render_template("contact_us.html", req_datetime=datetime.now())
+        else:
+            rname = request.form['contact-name']
+            remail = request.form['contact-email']
+            rneed = request.form['contact-need']
+            rmessage = request.form['contact-message']
+
+            if rmessage == "":
+                flash('Warning : No message included!', 'danger')
+                return redirect(url_for('contact_us'))
+            new_Req = Request(name=rname, email=remail, need=rneed, message=rmessage)
+            # print(new_Req)
+            db.session.add(new_Req)
+            db.session.commit()
+
+            flash('Message sent successfully!', 'success')
+            return redirect(url_for('contact_us'))
 
     @app.route("/covid19")
     def covid19():
@@ -52,59 +70,94 @@ def create_app(test_config=None):
 
     @app.route("/log_in", methods=['POST', 'GET'])
     def log_in():
-        return render_template("log_in.html", req_datetime=datetime.now())
+        if flask.request.method == 'GET':
+            return render_template("log_in.html", req_datetime=datetime.now())
+        else:
+            email = request.form['email']
+            password = request.form['password']
 
-    @app.route("/sign_up")
+            temp_user = User.query.filter_by(email=email).first()
+
+            if temp_user is not None and check_password_hash(temp_user.password, password):
+                flash('You have successfully logged in!', 'success')
+                login_user(temp_user)
+                return redirect(url_for('home'))
+            else:
+                flash('Wrong Credentials', 'danger')
+                return redirect(url_for('log_in'))
+
+    @app.route("/sign_up", methods=['POST', 'GET'])
     def sign_up():
-        return render_template("sign_up.html", req_datetime=datetime.now())
+        if flask.request.method == 'GET':
+            return render_template("sign_up.html", req_datetime=datetime.now())
+        else:
+            first_name = request.form['first_name']
+            last_name = request.form['last_name']
+            region_id = request.form['region']
+            email = request.form['email']
+            password = request.form['password']
+            confirm = request.form['confirm-password']
+
+            existing_user = User.query.filter_by(email=email).first()
+
+            if existing_user is None:
+                if password == confirm:
+                    new_user = User(first_name=first_name, last_name=last_name, region_id=region_id, email=email,
+                                    password=generate_password_hash(password, method='sha256'))
+                    db.session.add(new_user)
+                    db.session.commit()
+                    flash('Registration was Successful. Please Log in.', 'success')
+                    return redirect(url_for('log_in'))
+                flash('Passwords do not match. Please try again.', 'danger')
+                return redirect(url_for('sign_up'))
+
+            flash('Email address already exists.', 'danger')
+            return redirect(url_for('sign_up'))
 
     @app.route("/change_password", methods=['POST', 'GET'])
     def change_password():
-        return render_template("change_password.html", req_datetime=datetime.now())
+        if flask.request.method == 'GET':
+            return render_template("change_password.html", req_datetime=datetime.now())
+        else:
+            password = request.form['old_password']
+            new_password = request.form['new_password']
+            conf_new_pass = request.form['conf_new_password']
+
+            if check_password_hash(current_user.password, password) and new_password == conf_new_pass:
+                current_user.password = generate_password_hash(new_password, method='sha256')
+                db.session.add(current_user)
+                db.session.commit()
+                flash('Password changed successfully!', 'success')
+                return redirect(url_for('home'))
+            else:
+                flash('Something went wrong. Try again', 'danger')
+                return redirect(url_for('change_password'))
 
     @app.route("/account_details", methods=['POST', 'GET'])
     def account_details():
         return render_template("account_details.html", req_datetime=datetime.now())
 
-    @app.route("/signUp", methods=['POST', 'GET'])
-    def signUp():
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        region_id = request.form['region']
-        email = request.form['email']
-        password = request.form['password']
-        confirm = request.form['confirm-password']
-
-        existing_user = User.query.filter_by(email=email).first()
-
-        if existing_user is None:
-            if password == confirm:
-                new_user = User(first_name=first_name, last_name=last_name, region_id=region_id, email=email,
-                                password=generate_password_hash(password, method='sha256'))
-                db.session.add(new_user)
-                db.session.commit()
-                flash('Registration was Successful. Please Log in.', 'success')
-                return redirect(url_for('log_in'))
-            flash('Passwords do not match. Please try again.', 'danger')
-            return redirect(url_for('sign_up'))
-
-        flash('Email address already exists.', 'danger')
-        return redirect(url_for('sign_up'))
-
-    @app.route('/login', methods=['POST', 'GET'])
-    def login():
-        email = request.form['email']
-        password = request.form['password']
-
-        temp_user = User.query.filter_by(email=email).first()
-
-        if temp_user is not None and check_password_hash(temp_user.password, password):
-            flash('You have successfully logged in!', 'success')
-            login_user(temp_user)
-            return redirect(url_for('home'))
+    @app.route("/forgot_password", methods=['POST', 'GET'])
+    def forgot_password():
+        if flask.request.method == 'GET':
+            return render_template("forgot_password.html", req_datetime=datetime.now())
         else:
-            flash('Wrong Credentials', 'danger')
-            return redirect(url_for('log_in'))
+            email = request.form['email']
+            first_name = request.form['first_name']
+            last_name = request.form['last_name']
+            region = request.form['region']
+
+            temp_user = User.query.filter_by(email=email).first()
+
+            if temp_user.email == email and temp_user.first_name == first_name and temp_user.last_name == last_name and temp_user.region == region:
+                new_req = Request(email=temp_user.email, name=temp_user.last_name, need='-1', message='-1')
+                db.session.add(new_req)
+                db.session.commit()
+                flash('Your request has been submitted. Someone will contact you soon!', 'success')
+                return redirect(url_for('home'))
+            else:
+                flash('No user found with these credentials', 'danger')
+                return redirect(url_for('forgot_password'))
 
     @app.route('/logout')
     @login_required
@@ -115,40 +168,6 @@ def create_app(test_config=None):
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
-
-    @app.route("/contactUs", methods=['POST', 'GET'])
-    def contactUs():
-        rname = request.form['contact-name']
-        remail = request.form['contact-email']
-        rneed = request.form['contact-need']
-        rmessage = request.form['contact-message']
-
-        if rmessage == "":
-            flash('Warning : No message included!', 'danger')
-            return redirect(url_for('contact_us'))
-        new_Req = Request(name=rname, email=remail, need=rneed, message=rmessage)
-        # print(new_Req)
-        db.session.add(new_Req)
-        db.session.commit()
-
-        flash('Message sent successfully!', 'success')
-        return redirect(url_for('contact_us'))
-
-    @app.route("/changePassword", methods=['POST', 'GET'])
-    def changePassword():
-        password = request.form['old_password']
-        new_password = request.form['new_password']
-        conf_new_pass = request.form['conf_new_password']
-
-        if check_password_hash(current_user.password, password) and new_password == conf_new_pass:
-            current_user.password = generate_password_hash(new_password, method='sha256')
-            db.session.add(current_user)
-            db.session.commit()
-            flash('Password changed successfully!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Something went wrong. Try again', 'danger')
-            return redirect(url_for('change_password'))
 
     @app.route("/accountDetails", methods=['POST', 'GET'])
     def accountDetails():
@@ -174,6 +193,7 @@ def create_app(test_config=None):
         return render_template('error.html'), 400
 
     app.register_error_handler(400, page_not_found)
+
 
     return app
 
